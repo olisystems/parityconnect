9a670dea457b617496c52812c1033da8e8cc2dc3
m-yahya <yahya_gis@hotmail.com>
2019-08-02 13:20:18 +0200

    Add socket.io

---
+++ server.js
@@ -0,0 +1,24 @@
+// dependencies
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// set up server
+
+const app = express();
+const port = 80;
+app.listen(process.env.PORT || port);
+console.log('\n local server is running on port ' + port + '!');
+
+// set up app
+
+// public folder for static files
+
+app.use(express.static('dist'));
+
+// enable body parsing for json format
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
